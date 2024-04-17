08ea38e56849979efc3d99829837a0961a26b46e
HasenschwandtnerAndreas <113516687+HasenschwandtnerAndreas@users.noreply.github.com>
2024-04-17 14:11:56 +0200

    getting the userID and loggin now works

--- Website/public/script.ts
+++ Website/public/script.ts
@@ -1,8 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
    initLogin();
    initInput();
+   userID = await fetchRestEndpoint("/authenticated", "GET");
+   console.log(userID);
 });
 
+let userID;
 let loginBTN;
 let regBTN;
 
@@ -44,4 +47,22 @@ function initInput() {
             throw new Error("Invalid ID entered");
         }
     });
+}
+
+
+
+async function fetchRestEndpoint(route: string, method: "GET" |"POST" |"PUT" |"DELETE", data?: object): Promise<any> {
+    let options: any = { method };
+    if (data) {
+        options.headers = { "Content-Type": "application/json" };
+        options.body = JSON.stringify(data);
+    }
+    const res = await fetch(route, options);
+    if (!res.ok) {
+        const error = new Error(`${method} ${res.url} ${res.status} (${res.statusText})`);
+        throw error;
+    }
+    if (res.status !== 204) {
+        return await res.json();
+    }
 }
