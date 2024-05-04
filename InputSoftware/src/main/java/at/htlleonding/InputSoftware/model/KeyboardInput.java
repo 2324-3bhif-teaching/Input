fab83282176bb15821a89dcaa1d4be6f7edebc67
HasenschwandtnerAndreas <113516687+HasenschwandtnerAndreas@users.noreply.github.com>
2024-05-04 09:35:59 +0200

    implemented methods to move forward

--- InputSoftware/src/main/java/at/htlleonding/InputSoftware/model/KeyboardInput.java
+++ InputSoftware/src/main/java/at/htlleonding/InputSoftware/model/KeyboardInput.java
@@ -1,12 +1,10 @@
 package at.htlleonding.InputSoftware.model;
 
+import at.htlleonding.InputSoftware.model.Input;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import javax.swing.JFrame;
-
-public class KeyboardInput implements Input{
+public class KeyboardInput implements Input {
     private Scene mScene;
 
     public KeyboardInput(Scene scene) {
@@ -15,11 +13,59 @@ public class KeyboardInput implements Input{
 
     public void start() {
         mScene.setOnKeyPressed(event -> {
-            System.out.println("Key Pressed: " + event.getCode());
+            KeyCode keyCode = event.getCode();
+            switch (keyCode) {
+                case W:
+                    goForward();
+                    break;
+                case S:
+                    goBackward();
+                    break;
+                case A:
+                    steerLeft();
+                    break;
+                case D:
+                    steerRight();
+                    break;
+            }
+        });
+
+        mScene.setOnKeyReleased(event -> {
+            KeyCode keyCode = event.getCode();
+            switch (keyCode) {
+                case W:
+                case S:
+                    stopMoving();
+                    break;
+                case A:
+                case D:
+                    stopSteering();
+                    break;
+            }
         });
     }
+
+    private void goForward() {
+        System.out.println("Moving forward");
+    }
+
+    private void goBackward() {
+        System.out.println("Moving backward");
+    }
+
+    private void steerLeft() {
+        System.out.println("Steering left");
+    }
+
+    private void steerRight() {
+        System.out.println("Steering right");
+    }
+
+    private void stopMoving() {
+        System.out.println("Stopping movement");
+    }
+
+    private void stopSteering() {
+        System.out.println("Stopping steering");
+    }
 }
-
-
-
-
